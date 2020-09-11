b9617dc71c6f5952264babfa4a524c2d96558895
Anne Nardolilli <anardolilli@gmail.com>
2020-09-11 18:03:11 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,14 +1,14 @@
-def find_min_value(array)
+def find_max_value(array)
   count = 0 
-  min_value = -1
+  max_value = -1
   
   while count < array.length do 
-    if min_value > array[count]
-      min_value = array[count]
+    if max_value < array[count]
+      max_value = array[count]
     end
     count += 1 
 end
-min_value 
+max_value 
 end 
 fun = [1,2,6,7,3,9,1,2,2]
 find_min_value(fun)
